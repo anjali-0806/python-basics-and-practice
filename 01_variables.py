fe0d36925d929b16fee0f3ca83ccc9d867bc5bc9
ANJALI RAJPOOT <ANJALIRAJPUTKLD@GMAIL.COM>
2026-08-16 14:51:13 +0530

    Add initial variable definitions and print types

---
+++ 01_variables.py
@@ -0,0 +1,16 @@
+# Python Variables and Data Types
+
+name = "Anjali"
+age = 21
+height = 5.5
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
